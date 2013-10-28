12a6fe10ee7c286faec14ca596124e996ae7b0dc
xseignard <xavier.seignard@gmail.com>
2013-10-28 12:06:42 +0100

    Text review + some corrections
    Former-commit-id: 8b561c190271c0ade9061af8283c3fe3b04cc9a4

--- public/src/assets/js/slide.js
+++ public/src/assets/js/slide.js
@@ -1,68 +1,41 @@
-// $(document).ready(function() {
-// 	// $('.slideButton, button[type=button], .slideButton-illus').click(function() {
-// 	// 	var thisSlide = $(this).closest('ul.slide');
-// 	// 	thisSlide.toggleClass('slided');
-// 	// });
-
-// 	// $('#picto9, #picto-min-9, #picto-min-landscape-9').click(function() {
-// 	// 	var thisSlide = $(this).closest('#top').find('#section-00').children('ul.slide');
-// 	// 	thisSlide.toggleClass('slided');
-// 	// });
-
-	
-// });
-
-
 $(document).ready(function(){
-  var $slides	= $(".slide");
+  var $slides	= $('.slide');
   
-  TweenLite.set($slides.filter(":odd"), {left:"100%"});	
-    
-  // $(".slide:nth-child(odd)").on("click", function() {
-  //   var thisSlide = $(this).closest('.slide');
-  //   TweenLite.to(thisSlide, 1, {left:"-100%"});
-  //   TweenLite.to(thisSlide.next(), 1, {left:"0px"});
-  // });
-  
-  // $(".slide:nth-child(even)").on("click", function() {
-  //   var thisSlide = $(this).closest('.slide');
-  //   TweenLite.to(thisSlide, 1, {left:"100%"});
-  //   TweenLite.to(thisSlide.prev(), 1, {left:"0px"});
-  // });
+  TweenLite.set($slides.filter(':odd'), {left:'100%'});	
 
-$(".slideButton").on("click", function() {
+  $('.slideButton').on('click', function() {
     var thisSlide = $(this).parent().parent().parent('.slide');
     thisSlide.next().addClass('slide-show');
-    console.log(thisSlide);
-    TweenLite.to(thisSlide, 1, {left:"-100%"});
-    TweenLite.to(thisSlide.next(), 1, {left:"0px"});
+    TweenLite.to(thisSlide, 1, {left:'-100%'});
+    TweenLite.to(thisSlide.next(), 1, {left:'0px'});
   });
 
-$(".slideButton-illus").on("click", function() {
+  $('.slideButton-illus').on('click', function() {
     var thisSlide = $(this).parent().parent().parent('.slide');
     thisSlide.next().addClass('slide-show');
-    console.log(thisSlide);
-    TweenLite.to(thisSlide, 1, {left:"-100%"});
-    TweenLite.to(thisSlide.next(), 1, {left:"0px"});
+    TweenLite.to(thisSlide, 1, {left:'-100%'});
+    TweenLite.to(thisSlide.next(), 1, {left:'0px'});
   });
 
-  $("#picto9, .qui, #picto-min-9, #picto-min-landscape-9").on("click", function() {
+  $('#picto9, .qui, #picto-min-9, #picto-min-landscape-9').on('click', function() {
     var thisSlide = $(this).closest('#top').find('#section-00').find('.slide');
     thisSlide.next().addClass('slide-show');
-    console.log(thisSlide);
-    TweenLite.to(thisSlide, 1, {left:"-100%"});
-    TweenLite.to(thisSlide.next(), 1, {left:"0px"});
+    TweenLite.to(thisSlide, 1, {left:'-100%'});
+    TweenLite.to(thisSlide.next(), 1, {left:'0px'});
   });
   
-  $(".slide-button-2").on("click", function() {
+  $('.slide-button-2').on('click', function() {
     var thisSlide = $(this).parent().parent().parent('.slide');
-    TweenLite.to(thisSlide, 1, {left:"100%"});
-    TweenLite.to(thisSlide.prev(), 1, {left:"0px"});
+    TweenLite.to(thisSlide, 1, {left:'100%'});
+    TweenLite.to(thisSlide.prev(), 1, {left:'0px'});
   });
 
-  $("button[type=button]").on("click", function() {
+  $('button[type=button]').on('click', function() {
     var thisSlide = $(this).parent().parent().parent().parent('.slide');
-    TweenLite.to(thisSlide, 1, {left:"100%"});
-    TweenLite.to(thisSlide.prev(), 1, {left:"0px"});
+    TweenLite.to(thisSlide, 1, {left:'100%'});
+    TweenLite.to(thisSlide.prev(), 1, {left:'0px'});
+    // pause the video
+    var video = $(this).prev().find('video')[0];
+    if (video) video.pause();
   });
 });
