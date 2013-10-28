b5007cfa71c1ae9b01ed553ff1f1463c38c5a253
xseignard <xavier.seignard@gmail.com>
2013-10-28 14:01:08 +0100

    Modif textes + lazy load social

--- public/dist/assets/js/main.js
+++ public/dist/assets/js/main.js
@@ -164,4 +164,22 @@ $(document).ready(function() {
     var video = $(this).prev().find('video')[0];
     if (video) video.pause();
   });
-});
+});;(function(doc, script) {
+  var js, 
+      fjs = doc.getElementsByTagName(script)[0],
+      frag = doc.createDocumentFragment(),
+      add = function(url, id) {
+          if (doc.getElementById(id)) {return;}
+          js = doc.createElement(script);
+          js.src = url;
+          id && (js.id = id);
+          frag.appendChild( js );
+      };
+      
+    // Facebook SDK
+    add('//connect.facebook.net/en_US/all.js#xfbml=1', 'facebook-jssdk');
+    // Twitter SDK
+    add('//platform.twitter.com/widgets.js');
+
+    fjs.parentNode.insertBefore(frag, fjs);
+}(document, 'script'));
